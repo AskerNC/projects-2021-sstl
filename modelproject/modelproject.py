b53c88240cecac251e03f11509cd53b4fe47525b
Stine <qvj635@alumni.ku.dk>
2021-05-09 12:16:30 +0200

    +

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,25 +1,43 @@
-from scipy import optimize
-
-def solve_for_ss(s,g,n,alpha,delta):
-    """ solve for the steady state level of capital
-
+def simulate_basic(k,alpha,delta,s,n,g,K,T,D):
+    """
     Args:
-        s (float): saving rate
-        g (float): technological growth rate
-        n (float): population growth rate
-        alpha (float): cobb-douglas parameter
-        delta (float): capital depreciation rate 
-
+    k      (int)  : initial value for physical capital per worker
+    alpha  (float): return on physical capital
+    delta  (float): depreciation rate on capital
+    s      (float): savings/investments
+    n      (float): increase in labour
+    g             : growth
+    K      (int)  : totalfactorproductivity
+    T      (int)  : periods to simulate through
+    D             : climate change
+    
     Returns:
-        result (RootResults): the solution represented as a RootResults object
-
-    """ 
+    A plot showing how capital per worker accumulates over time"""
     
-    # a. define objective function
-    f = lambda k: k**alpha
-    obj_kss = lambda kss: kss - (s*f(kss) + (1-delta)*kss)/((1+g)*(1+n))
-
-    #. b. call root finder
-    result = optimize.root_scalar(obj_kss,bracket=[0.1,100],method='bisect')
+    #First we make a list for our future values to be in
+    k_path = [k]
     
-    return result
+    #Then we make which will contain the values for the 45-degree-line
+    Degreeline = [0]
+    
+    #We generate the 45-degree-line for the basic Solow model
+    for t in range(1,T):
+        line = (n+g+delta)*t
+        Degreeline.append(line)
+        
+    #We generate the Solow movement
+    for t in range(1,T):
+        k_plus = s*(1-D)*K*t**alpha
+        k_path.append(k_plus)
+    
+    #Plotting the stuff
+    plt.figure(figsize=(7,7))
+    plt.plot(k_path[:T], label='$s(1-D)k_t^{\u03B1}$', color = 'red')
+    plt.plot(Degreeline[:T], label = '$k_t(n+\delta+g)$', color = 'blue')
+    plt.xlim(0,T)
+    plt.ylim(0,Degreeline[-1])
+    plt.xlabel('$k_t$')
+    plt.grid(True)
+    plt.legend()
+    
+    return plt.show()
